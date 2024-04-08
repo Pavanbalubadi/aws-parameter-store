7463830c531c5d04331abeb16c240cfa69f72d4f
pavan <balubadipavan@gmail.com>
2024-04-08 08:34:46 +0530

    TF code rds

--- vars.tf
+++ vars.tf
@@ -3,7 +3,7 @@ variable "parameters" {
      { name = "dev.backend.DB_HOST", type = "String", value = "dev-msql-rds.c9sgww8wun07.us-east-1.rds.amazonaws.com" },
      { name = "dev.frontend.BACKEND_ENDPOINT", type = "String", value = "http://backend-dev.rdevopsb72.online/" },
     ##RDS
-    {name="dev.rds.username" ,type="string" ,value="admin1"},
+    {name="dev.rds.username" ,type="String" ,value="admin1"},
     { name = "dev.rds.endpoint", type = "String", value = "dev-msql-rds.c9sgww8wun07.us-east-1.rds.amazonaws.com" },
 
 
